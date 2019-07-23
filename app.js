e3900ff9236b4d4a1b41bb97539680e6f51a989c
pranavpsv <30323565+pranavpsv@users.noreply.github.com>
2019-07-24 01:04:40 +0530

    Add access to individual webpages of each post

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "This is Pranav's personal blog.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+var content = "";
 const app = express();
-const posts = []
+const posts = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -37,18 +38,16 @@ app.get("/post", (req, res) => {
 
 app.get("/posts/:title", (req, res) => {
   let title = req.params.title;
-  let matchFound = false;
-  posts.forEach(item => {
-    if (title == item.title) {
-      matchFound = true;
+  let matched = false;
+  posts.forEach(post => {
+    if (title == post.title) {
+      content = post.content;
+      console.log(content);
+      matched = true; 
     }
   });
-  if (matchFound == true) {
-    console.log("Match found!");
-  } else {
-    console.log("Match not found!");
-  }
-  res.render("post", {postTitle: title, postContent: 0});
+  console.log(matched);
+  res.render("post", {postTitle: title, postContent: content});
 });
 
 app.post("/", (req, res) => {
